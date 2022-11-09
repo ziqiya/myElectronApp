4f03d17123cdda86777aa2fb72f254a666d06edb
ruanxusong <ruanxusong@thundersdata.com>
2022-11-09 10:27:44 +0800

    fix: 新增windows打包

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   // 创建浏览器窗口
@@ -6,14 +6,28 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
   // 加载index.html文件
   // win.loadFile("index.html");
   // 加载博客页面
-  win.loadURL("https://www.ruanxusong.com");
+  win.loadURL('http://114.106.163.51:33067/');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  // 如果没有窗口打开则打开一个窗口(mac)
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// window/linux 关闭所有窗口时退出应用
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
